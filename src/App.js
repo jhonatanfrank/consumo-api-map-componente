2e75d073487a1b35cf280d29a195fe14377c902f
jhonatanfrank <frank_mch97@hotmail.com>
2023-01-04 13:31:01 -0500

    consumo-api-map-componente

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import Pokemon from './Components/Pokemon';
+
 
 function App() {
+
+  const [equipo, setEquipo] = useState([])
+
+  useEffect(() => {
+    obtenerDatos()
+  }, [])
+
+  const obtenerDatos = async () => {
+    //const data = await fetch("https://jsonplaceholder.typicode.com/users")
+    const data = await fetch("https://jsonplaceholder.typicode.com/users/")
+
+    const users = await data.json()
+    console.log(users)
+    setEquipo(users)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <h1>CONSUMO DE API CON MAP PASANDO A UN COMPONENTE</h1>
+        <>
+          {equipo.map((parametro, index) =>
+            <Pokemon
+              key={index}
+              id={parametro.id}
+              url={parametro.url}
+              name={parametro.name}
+              nombre="frank"
+            />)}
+        </>
+      </div>
+    </>
   );
 }
 
---
+++ src/Components/Pokemon.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Pokemon = ({ id, name }) => {
+    return (
+        <>
+            <div>{id}</div>
+            <div>{name}</div>
+        </>
+    )
+}
+
+export default Pokemon
